32f38429518980f3b341b6e2c4317a18893b04b1
adithya20-afk <adithya.kp@s.amity.edu>
2025-05-18 22:54:57 +0530

    commit in branch1|file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -5,27 +5,33 @@ using namespace std;
 
 int main() {
     // Declare variables
-    string name;
-    int age;
+    string n;
+    int age,a,b,sum;
     double height;
 
     // Prompt user for their name
-    cout << "Enter your name: ";
-    getline(cin, name); // Allows input of full name with spaces
+    cout << "Enter your sweet name: ";
+    getline(cin, n); // Allows input of full name with spaces
 
     // Prompt user for their age
-    cout << "Enter your age: ";
+    cout << "Enter your sweet age: ";
     cin >> age;
 
     // Prompt user for their height in centimeters
-    cout << "Enter your height (in cm): ";
+    cout << "what's your height (in cm): ";
     cin >> height;
 
     // Display the collected information
     cout << "\n--- User Information ---" << endl;
-    cout << "Name: " << name << endl;
+    cout << "Name: " << n << endl;
     cout << "Age: " << age << " years" << endl;
     cout << "Height: " << height << " cm" << endl;
+
+    //adding two numbers
+    cout<<"enter any two numbers: ";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum of the entered two numbers is "<<sum;
 
     return 0;
 }
